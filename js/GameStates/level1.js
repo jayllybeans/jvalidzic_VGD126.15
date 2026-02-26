b1ecbfba23b1c5373de2b0c3b6d889889b03b4ba
Jay Validzic <jmvalidzic@email.neit.edu>
2026-02-26 11:38:04 -0500

    Updates to sound

--- js/GameStates/level1.js
+++ js/GameStates/level1.js
@@ -109,7 +109,7 @@ gameStates[`level1`] = function()
 	{
 		wiz.top={x:0,y:0};
 		wiz.changeState(`crouch`)
-		sounds.play(`nuhuh`,1)
+		sounds.play(`nuhuh`, 0)
 	}
 	else
 	{
@@ -142,7 +142,7 @@ gameStates[`level1`] = function()
 		wiz.canJump = false;
 		wiz.vy = wiz.jumpHeight;
 		wiz.changeState(`jump`)
-		sounds.play(`splode`,1)
+		sounds.play(`splode`, 0)
 	}
 	shotTimer--;
 	if(shotTimer <=0)
@@ -168,7 +168,7 @@ gameStates[`level1`] = function()
 			bullets[currentBullet].y = wiz.y + 20;
 			bullets[currentBullet].dir = wiz.dir;
 			
-			//sounds.play(`splode`,1)
+			sounds.play(`yell`,0)
 
 			currentBullet++;
 			if(currentBullet>=bullets.length)
